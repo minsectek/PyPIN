fc9b030348e335e238b6573210e94b6784dce20b
Min SecuriTek <minsec@minsectek.com>
2017-07-12 22:30:37 -0700

    Add files via upload

---
+++ PyPIN.py
@@ -0,0 +1,39 @@
+pin = 0
+
+def uptofour():
+	pin = 0
+	print(pin)
+	while pin < 9999:
+		pin = pin + 1
+		print(pin)
+
+
+
+def allfour():
+	pin = 1000
+	print(pin)
+	while pin < 9999:
+		pin = pin + 1
+		print(pin)
+
+
+
+def uptoeight():
+	pin = 0
+	print(pin)
+	while pin < 99999999:
+		pin = pin + 1
+		print(pin)
+
+
+def alleight():
+	pin = 10000000
+	print(pin)
+	while pin < 99999999:
+		pin = pin + 1
+		print(pin)
+
+
+
+
+
